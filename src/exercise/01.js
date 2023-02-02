6cbdf828ac645ed7dcb9f5ef93684acfd9caec44
Soobin Son <ssbin93@gmail.com>
2023-02-02 22:25:22 +1000

    do exercise 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,18 +1,18 @@
 // Context Module Functions
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
-import {dequal} from 'dequal'
+import React, { createContext, useReducer, useState } from 'react';
+import { dequal } from 'dequal';
 
 // ./context/user-context.js
 
-import * as userClient from '../user-client'
-import {useAuth} from '../auth-context'
+import * as userClient from '../user-client';
+import { useAuth } from '../auth-context';
 
-const UserContext = React.createContext()
-UserContext.displayName = 'UserContext'
+const UserContext = createContext();
+UserContext.displayName = 'UserContext';
 
-function userReducer(state, action) {
+const userReducer = (state, action) => {
   switch (action.type) {
     case 'start update': {
       return {
@@ -20,7 +20,7 @@ function userReducer(state, action) {
         user: {...state.user, ...action.updates},
         status: 'pending',
         storedUser: state.user,
-      }
+      };
     }
     case 'finish update': {
       return {
@@ -29,7 +29,7 @@ function userReducer(state, action) {
         status: 'resolved',
         storedUser: null,
         error: null,
-      }
+      };
     }
     case 'fail update': {
       return {
@@ -38,72 +38,80 @@ function userReducer(state, action) {
         error: action.error,
         user: state.storedUser,
         storedUser: null,
-      }
+      };
     }
     case 'reset': {
       return {
         ...state,
         status: null,
         error: null,
-      }
+      };
     }
     default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
+      throw new Error(`Unhandled action type: ${action.type}`);
     }
   }
-}
+};
 
-function UserProvider({children}) {
-  const {user} = useAuth()
-  const [state, dispatch] = React.useReducer(userReducer, {
+const UserProvider = ({ children }) => {
+  const { user } = useAuth();
+  const [state, dispatch] = useReducer(userReducer, {
     status: null,
     error: null,
     storedUser: user,
     user,
-  })
-  const value = [state, dispatch]
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+  });
 
-function useUser() {
-  const context = React.useContext(UserContext)
+  const value = [state, dispatch];
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+const useUser = () => {
+  const context = React.useContext(UserContext);
+
   if (context === undefined) {
-    throw new Error(`useUser must be used within a UserProvider`)
+    throw new Error(`useUser must be used within a UserProvider`);
   }
-  return context
-}
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+  return context;
+};
+
+const updateUser = async (dispatch, user, updates) => {
+  dispatch({ type: 'start update', updates });
+
+  try {
+    const updatedUser = await userClient.updateUser(user, updates);
+    dispatch({ type: 'finish update', updatedUser });
+    return updatedUser;
+  } catch (error) {
+    dispatch({ type: 'fail update', error });
+    throw error;
+  }
+};
 
 // export {UserProvider, useUser}
 
 // src/screens/user-profile.js
 // import {UserProvider, useUser} from './context/user-context'
-function UserSettings() {
-  const [{user, status, error}, userDispatch] = useUser()
+const UserSettings = () => {
+  const [{ user, status, error }, userDispatch] = useUser();
 
-  const isPending = status === 'pending'
-  const isRejected = status === 'rejected'
+  const isPending = status === 'pending';
+  const isRejected = status === 'rejected';
 
-  const [formState, setFormState] = React.useState(user)
+  const [formState, setFormState] = useState(user);
 
-  const isChanged = !dequal(user, formState)
+  const isChanged = !dequal(user, formState);
 
-  function handleChange(e) {
+  const handleChange = (e) => {
     setFormState({...formState, [e.target.name]: e.target.value})
-  }
+  };
 
-  function handleSubmit(event) {
-    event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateUser(userDispatch, user, formState);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -170,31 +178,29 @@ function UserSettings() {
         {isRejected ? <pre style={{color: 'red'}}>{error.message}</pre> : null}
       </div>
     </form>
-  )
+  );
+};
+
+const UserDataDisplay = () => {
+  const [{ user }] = useUser();
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
 }
 
-function UserDataDisplay() {
-  const [{user}] = useUser()
-  return <pre>{JSON.stringify(user, null, 2)}</pre>
-}
-
-function App() {
-  return (
-    <div
-      style={{
-        minHeight: 350,
-        width: 300,
-        backgroundColor: '#ddd',
-        borderRadius: 4,
-        padding: 10,
-      }}
-    >
-      <UserProvider>
-        <UserSettings />
-        <UserDataDisplay />
-      </UserProvider>
-    </div>
-  )
-}
+const App = () => (
+  <div
+    style={{
+      minHeight: 350,
+      width: 300,
+      backgroundColor: '#ddd',
+      borderRadius: 4,
+      padding: 10,
+    }}
+  >
+    <UserProvider>
+      <UserSettings />
+      <UserDataDisplay />
+    </UserProvider>
+  </div>
+);
 
 export default App
